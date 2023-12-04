30e498c1b6f448b6b5e2085e944751b247cf46a2
Anna Wang <annawang125@gmail.com>
2023-12-03 21:15:00 -0800

    Change playlists header styling, remove add button

--- components/Playlists.tsx
+++ components/Playlists.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { AiOutlinePlus } from 'react-icons/ai';
 
 import useToken from '@/hooks/useToken';
 import fetchWebApi from '@/utils/fetchWebApi';
@@ -23,20 +22,10 @@ const Playlists = () => {
     getPlaylists();
   }, [token]);
 
-  const onClick = () => {
-    // handle upload
-  };
   return (
     <div className='h-full flex flex-col gap-4'>
-      <div className='flex items-center justify-between'>
-        <div className='inline-flex items-center gap-x-2'>
-          <h2 className='text-neutral-400 font-medium text-md'>Playlists</h2>
-        </div>
-        <AiOutlinePlus
-          onClick={onClick}
-          size={20}
-          className='text-neutral-400 cursor-pointer hover:text-white transition'
-        />
+      <div className='inline-flex items-center gap-x-2'>
+        <h2 className='text-neutral-400 uppercase text-sm'>Playlists</h2>
       </div>
       <div className='flex flex-col gap-2 overflow-y-scroll'>
         {playlists &&
