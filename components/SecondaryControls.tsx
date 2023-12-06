e83c6d1184125a44bac5620714adc35e31ffe2e8
Anna Wang <annawang125@gmail.com>
2023-12-05 18:46:17 -0800

    Delete queue functionality because API endpoint doesn't work properly

--- components/SecondaryControls.tsx
+++ components/SecondaryControls.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { PiQueue as Queue } from 'react-icons/pi';
 import { GoMute as Muted, GoUnmute as Unmuted } from 'react-icons/go';
 import Slider from '@mui/material/Slider';
 
@@ -14,46 +13,37 @@ const SecondaryControls = ({
   const [isMuted, setIsMuted] = useState(false);
 
   return (
-    <>
+    <div className='flex items-center gap-2'>
       <button
         onClick={() => {
-          // TODO: display queue
+          if (isMuted) {
+            setIsMuted(false);
+            updateVolume(volume / 100);
+          } else {
+            setIsMuted(true);
+            updateVolume(0);
+          }
         }}
       >
-        <Queue size={24} title='Queue' />
+        {isMuted ? (
+          <Muted size={24} title='Unmute' />
+        ) : (
+          <Unmuted size={24} title='Mute' />
+        )}
       </button>
-      <div className='flex items-center gap-2'>
-        <button
-          onClick={() => {
-            if (isMuted) {
-              setIsMuted(false);
-              updateVolume(volume / 100);
-            } else {
-              setIsMuted(true);
-              updateVolume(0);
-            }
-          }}
-        >
-          {isMuted ? (
-            <Muted size={24} title='Unmute' />
-          ) : (
-            <Unmuted size={24} title='Mute' />
-          )}
-        </button>
-        <Slider
-          aria-label='Volume'
-          value={isMuted ? 0 : volume}
-          onChange={(_, newValue) => {
-            setVolume(newValue as number);
-          }}
-          onChangeCommitted={(_, newValue) => {
-            setVolume(newValue as number);
-            updateVolume((newValue as number) / 100);
-          }}
-          className='w-24 text-white'
-        />
-      </div>
-    </>
+      <Slider
+        aria-label='Volume'
+        value={isMuted ? 0 : volume}
+        onChange={(_, newValue) => {
+          setVolume(newValue as number);
+        }}
+        onChangeCommitted={(_, newValue) => {
+          setVolume(newValue as number);
+          updateVolume((newValue as number) / 100);
+        }}
+        className='w-24 text-white'
+      />
+    </div>
   );
 };
 
