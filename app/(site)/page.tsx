87bfa0475c068d172dd7aebc7943bec89d4f9801
annawng <annawang125@gmail.com>
2023-12-14 00:05:24 -0800

    Remove query params from redirect

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,10 +3,16 @@
 import Homepage from '@/components/Homepage';
 
 import { useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Home = () => {
   const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/');
+  }, [router]);
 
   if (status === 'unauthenticated') {
     redirect('/login');
