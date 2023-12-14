c0617a9a840d5d04bd5cc459b8c8d1011fa408a9
Anna Wang <anna@Annas-MacBook-Pro.local>
2023-12-13 20:50:33 -0800

    Fix collection header mobile layout

--- components/CollectionHeader.tsx
+++ components/CollectionHeader.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import H1 from './H1';
+import useWindowDimensions from '@/hooks/useWindowDimensions';
 
 const CollectionHeader = ({
   name,
@@ -12,22 +13,27 @@ const CollectionHeader = ({
   isPlaylist: boolean;
   image: string;
 }) => {
+  const { width } = useWindowDimensions();
+  const isMobile = width < 768;
+
   return (
-    <div className='flex items-end gap-8 mb-6 max-w-full'>
+    <div className='flex flex-col md:flex-row items-center md:items-end gap-3 md:gap-8 mb-6 max-w-full'>
       <Image
         src={image}
         alt=''
-        width={240}
-        height={240}
+        width={isMobile ? 216 : 240}
+        height={isMobile ? 216 : 240}
         className='aspect-square object-cover'
         priority
       />
-      <div className='flex flex-col gap-2 min-w-0'>
-        <p className='text-neutral-400 uppercase text-sm'>
+      <div className='self-start md:self-end flex flex-col md:gap-2 min-w-0'>
+        <p className='hidden md:block text-neutral-400 uppercase text-sm'>
           {isPlaylist ? 'Playlist' : 'Album'}
         </p>
-        <H1 className='text-ellipsis overflow-hidden leading-tight'>{name}</H1>
-        <p className='font-medium'>{owner}</p>
+        <H1 className='text-xl text-ellipsis overflow-hidden leading-tight'>
+          {name}
+        </H1>
+        <p className='text-sm md:font-medium'>{owner}</p>
       </div>
     </div>
   );
